be4ee9f06c5ada4df4f4d81be890b33494f72944
rcvetko1 <rcvetko1@volvo-cars.com>
2023-03-01 11:12:11 +0100

    created powershell file

---
+++ create-backend-st.ps1
@@ -0,0 +1,41 @@
+$RESOURCE_GROUP_NAME = "backend-st-dev-rg"
+$STORAGE_ACCOUNT_NAME = "backendterrastdev01"
+$CONTAINER_NAME = "tfstate"
+
+# Login to Azure
+az login
+
+# Create resource group
+az group create `
+    --name $RESOURCE_GROUP_NAME `
+    --location "West Europe"
+
+# Create storage account
+az storage account create `
+    --resource-group $RESOURCE_GROUP_NAME `
+    --name $STORAGE_ACCOUNT_NAME `
+    --sku Standard_LRS `
+    --encryption-services blob
+
+# Get storage account key
+$ACCOUNT_KEY = $(az storage account keys list `
+        --resource-group $RESOURCE_GROUP_NAME `
+        --account-name $STORAGE_ACCOUNT_NAME `
+        --query [0].value -o tsv)
+
+# Create blob container
+az storage container create `
+    --name $CONTAINER_NAME `
+    --account-name $STORAGE_ACCOUNT_NAME `
+    --account-key $ACCOUNT_KEY
+
+# echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
+# echo "container_name: $CONTAINER_NAME"
+# echo "access_key: $ACCOUNT_KEY"
+
+
+# Create Service Principal
+# az ad sp create-for-rbac `
+#     --role="Contributor" `
+#     --scopes="/subscriptions/SUBSCRIPTION_ID" `
+#     --name="GitHub-Actions"
