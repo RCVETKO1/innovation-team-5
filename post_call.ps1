dd98a5e8975558cb229e64f80e4c80f655eff1f5
rcvetko1 <rcvetko1@volvo-cars.com>
2023-03-02 16:05:21 +0100

    modified

--- post_call.ps1
+++ post_call.ps1
@@ -3,11 +3,13 @@ $headers = @{
     "Authorization" = "token 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"
 }
 
+$uri = "https://api.github.com/repos/RCVETKO1/innovation-team-5/dispatches"
+
 $body = @{
     event_type = "do-something"
 }
 $body_json = $body | ConvertTo-Json
 
-$response = Invoke-RestMethod -Method POST -Headers $headers -Body $body_json -Uri "https://api.github.com/repos/RCVETKO1/innovation-team-5/dispatches/"
+$response = Invoke-RestMethod -Method 'Post' -Uri $uri -Headers $headers -Body $body_json
 
 $response
