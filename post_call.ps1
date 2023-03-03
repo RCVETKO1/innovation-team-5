f0131efe80dd963c9350a91c70d439764ebdb2a2
rcvetko1 <rcvetko1@volvo-cars.com>
2023-03-03 10:02:25 +0100

    added trigger event on terraform template

--- post_call.ps1
+++ post_call.ps1
@@ -1,15 +1,14 @@
 $headers = @{
     "Accept"        = "application/vnd.github+json"
-    "Authorization" = "token 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"
+    "Authorization" = "token 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"
 }
 
 $uri = "https://api.github.com/repos/RCVETKO1/innovation-team-5/dispatches"
 
 $body = @{
-    event_type = "do-something"
+    event_type = "run-template"
 }
 $body_json = $body | ConvertTo-Json
 
 $response = Invoke-RestMethod -Method 'Post' -Uri $uri -Headers $headers -Body $body_json
-
 $response
