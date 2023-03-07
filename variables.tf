3cae4612b58e150e915655d15299fa493c6609fe
rcvetko1 <rcvetko1@volvo-cars.com>
2023-03-07 14:37:05 +0100

    changed service name

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "location" {
 
 variable "service" {
   type        = string
-  default     = "innovation2"
+  default     = "innovation"
   description = "Definition of application deployment is subjected to."
 }
 
